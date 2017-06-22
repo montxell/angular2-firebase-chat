6205bd0e9d0be5d5bdca268e8c0d363c4671ab60
Montse Ariso <montxelltech@gmail.com>
2017-06-22 19:01:42 +0200

    Add code relative to the installation of AngularFire2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBtucmHqmMqhjZ8Wdc6u160vuljsmFMl0M",
+    authDomain: "firechat-78347.firebaseapp.com",
+    databaseURL: "https://firechat-78347.firebaseio.com",
+    projectId: "firechat-78347",
+    storageBucket: "firechat-78347.appspot.com",
+    messagingSenderId: "439559956872"
+  }
 };
