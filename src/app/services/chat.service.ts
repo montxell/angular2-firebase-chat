dee57db01974514051c3aa58e88d825dbb1868bc
Montse Ariso <montxelltech@gmail.com>
2017-07-02 07:06:55 +0200

    Add 'login' and 'logout' methods to service with Google/Twitter options

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@angular/core';
 
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
 import { Message } from '../interfaces/message.interface';
+
+import * as firebase from 'firebase/app';
+
 
 @Injectable()
 export class ChatService {
 
   chats: FirebaseListObservable<any[]>;
-  user: any = {
-    name: "Montxell"
-  };
+  user: any = {};
 
-  constructor( private db: AngularFireDatabase ) {
-    // this.chats = db.list('/chats');
+  constructor( private db: AngularFireDatabase,
+               private afAuth: AngularFireAuth ) {
+
+    if ( localStorage.getItem('user') ) {
+      // User logged
+      this.user = JSON.parse(localStorage.getItem('user'));
+
+    } else {
+
+      this.user = null;
+
+    }
   }
 
 
@@ -37,6 +50,53 @@ export class ChatService {
     }
 
     return this.chats.push( message ); //returns a promise
+  }
+
+
+
+  login( selectedProvider: string ) {
+
+    let provider;
+
+    if ( selectedProvider == "google" ) {
+
+      provider = new firebase.auth.GoogleAuthProvider();
+
+      provider.addScope('profile');
+      provider.addScope('email');
+
+    } else {
+
+      provider = new firebase.auth.TwitterAuthProvider();
+
+    }
+
+    // Using a popup.
+    firebase.auth().signInWithPopup(provider).then( (result) => {
+
+     console.log( result );
+
+     // This gives you a Google or Twitter Access Token.
+     let token = result.credential.accessToken;
+
+     // This gives you a Twitter API secret.
+     let secret = result.credential.secret;
+
+     // The signed-in user info.
+     this.user = result.user;
+
+     localStorage.setItem('user', JSON.stringify(result));
+
+    });
+  }
+
+
+  logout() {
+
+    localStorage.removeItem('user');
+    this.user = null;
+    this.afAuth.auth.signOut();
+
   }
 
 }
