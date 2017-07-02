57f8cb36dc2d7bd1e41cd6abb885c63bbfc4dd25
Montse Ariso <montxelltech@gmail.com>
2017-07-02 07:08:46 +0200

    Add 'chat' service to 'app.component.ts'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
+import { ChatService } from './services/chat.service';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,7 +11,11 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 })
 export class AppComponent {
   chats: FirebaseListObservable<any[]>;
-  constructor(db: AngularFireDatabase) {
+  
+  constructor( private db: AngularFireDatabase,
+               public _cs: ChatService) {
+
     this.chats = db.list('/chats');
+
   }
 }
